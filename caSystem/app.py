fbf8dd4bcc4c08d0b2bb0bd5468b15cd57578b4f
ken82 <ken.h.0802@gmail.com>
2019-08-16 16:38:07 +0900

    update

--- caSystem/app.py
+++ caSystem/app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+#! coding:utf-8
+# Manage application and rooting (includes main script of the application)
+from flask import Flask, render_template, request
 app = Flask(__name__)  # Generate Flask instance.
 
-# Decorator (root)
+# Decorator
 @app.route('/')
 def index():
-    return render_template('index.html')  # template file.
+    return render_template('index.html')  # first view (template file).
+
+# Main script
+@app.route('/app', methods=['POST'])  # form access by POST.
+def creeibility_assessment():  # This function is the main process of credibility assessment.
+    target = request.form['target']  # get the input from users.
+    return render_template('output.html', input = target)
+
 
 if __name__=='__main__':
     app.debug = True  # debug mode ON.
-    app.run('host=localhost')
+    app.run(host='localhost')  # run localhost.
