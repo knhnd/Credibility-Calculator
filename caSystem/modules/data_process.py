9b82b2ea5ec02a1eb66e341b48149c4f4418dcf2
ken82 <ken.h.0802@gmail.com>
2019-08-21 17:39:43 +0900

    update

---
+++ caSystem/modules/data_process.py
@@ -0,0 +1,72 @@
+#!/usr/local/bin/python3
+#_*_coding:utf-8_*_
+# Database operation module
+import os, re
+import json
+import sqlite3
+import subprocess
+
+# ディレクトリの操作
+def directory():
+    # 探索するディレクトリを指定("./dataset")するとループ一回毎に中のディレクトリを取得しdirへ
+    # その中のサブディレクトリをリストにしてsubdirへ, その下のファイルを全てfilesへ(今回はsubdirはない)
+    for dir, subdir, files in os.walk("./dataset/rumors"):
+        if dir == "./dataset/rumors/ottawa_shooting":  # 指定したディレクトリなら(germanwings_crash, ottawa_shooting, sydney_siege)
+            for file_name in files:  # 中のファイルを取得していく(全てstrなので実際のデータではない)
+                print(file_name)
+    return "Success!"
+#directory()
+
+# jsonデータの処理
+def json_processing(file_name):
+    json_file = file＿name  # JSONファイルを指定
+    #json_file = "./dataset/rumors/ottawa_shooting" + file_name  # JSONファイルのパスを指定(パスを手動で入れる場合)
+    json_data = json.load(open(json_file))  # jsonファイルを開く
+    json_time = json_data["created_at"]  # jsonデータ内の時間(created_at)を取得
+    json_text = json_data["text"]  # jsonデータ内の本文(text)を取得
+    json_dataset = [json_time, json_text]  # 取得したjsonデータの時間と本文をセットにする
+    print(json_dataset)
+    return "Success!"
+#json_processing()
+
+# 指定したファイルを別のディレクトリに移動する
+def data_move():
+    for dir, subdir, files in os.walk("./dataset/rumors/ottawa_shooting"):  # ディレクトリを指定
+        if dir.find("/source-tweet") != -1:
+            dir_id = dir
+            dir_id = dir_id.replace("./dataset/rumors/ottawa_shooting/", "")  # strで取得しているので正規表現で不要な部分を削除
+            dir_id = dir_id.replace("/source-tweet", "")
+            file_name = dir_id+".json"  # ディレクトリ名をファイル名に
+            path = "./dataset/rumors/ottawa_shooting/"+dir_id+"/source-tweet/"+file_name  # 操作したいファイルまでのPATH
+            move_dir = "./dataset/rumors/ottawa_shooting/"  # 移動先ディレクトリ
+            cmd = ["mv", path, move_dir] # 実行する外部コマンド(subprocessはスペースを読み込めないのでリストで繋げる)
+            subprocess.call(cmd)  # 外部コマンド実行
+    return "Success!"
+#data_move()
+
+# db操作
+def json_to_db(json_dataset):
+    db = sqlite3.connect('./credibility_assessment.db')  # dbへ接続
+    db.execute("insert into rumor_germanwings(time, data, source_id) values (?,?,?)", json_dataset)  # セットした json_dataset を db にinsert
+    db.commit()  # 変更をデータベースに保存
+    db.close()  # データベースとの接続解除
+    return "Success!"
+#json_to_db()
+
+# ディレクトリ内のjsonを取得して特定のデータを取り出しdbに保存
+def dir_json_db():
+    # 上の関数を組み合わせる
+    for dir, subdir, files in os.walk("./dataset/rumors"):
+        if dir == "./dataset/rumors/ottawa_shooting":
+            for file_name in files:
+                json_file = "./dataset/rumors/ottawa_shooting/" + file_name  # os.walkは中身を見るだけでアクセスするわけではないのでファイルにアクセスするためにはそのファイル名を指定する必要がある
+                json_data = json.load(open(json_file))
+                json_time = json_data["created_at"]
+                json_text = json_data["text"]
+                json_dataset = [json_time, json_text]
+                db = sqlite3.connect('./credibility_assessment.db')
+                db.execute("insert into rumor_ottawashooting(time, data) values (?,?)", json_dataset)
+                db.commit()  # 変更をデータベースに保存
+                db.close()  # データベースとの接続解除
+    return "Success!"
+#dir_json_db()
---
+++ caSystem/modules/db_operation.py
@@ -0,0 +1,31 @@
+#!/usr/local/bin/python3
+#_*_coding:utf-8_*_
+# Database operation module
+import sqlite3
+
+# Particular data retrieval from DB (get the table name and retrieve name). DB内の特定データの検索(テーブル名と検索キーワードを受け取る)
+# (Module: sqlite3)
+def search(table_name, keyword):
+    db = sqlite3.connect('./database/credibility_assessment.db')  # connect DB.
+    query = "select * from " + table_name + " where data like ?"  # query.
+    cmd = db.execute(query,('%' + keyword + '%',))  # execute SQL.
+    return cmd
+    db.close()  # disconnect from DB.
+
+# Get the particular data from json in directory and store the DB. ディレクトリ内のjsonを取得して特定のデータを取り出しdbに保存
+# (Module: sqlite3)
+def dir_json_db():
+    for dir, subdir, files in os.walk("./dataset/rumors"):
+        if dir == "./dataset/rumors/ottawa_shooting":
+            for file_name in files:
+                json_file = "./dataset/rumors/ottawa_shooting/" + file_name  # os.walkは中身を見るだけでアクセスするわけではないのでファイルにアクセスするためにはそのファイル名を指定する必要がある
+                json_data = json.load(open(json_file))
+                json_time = json_data["created_at"]
+                json_text = json_data["text"]
+                json_dataset = [json_time, json_text]
+                db = sqlite3.connect('./credibility_assessment.db')
+                db.execute("insert into rumor_ottawashooting(time, data) values (?,?)", json_dataset)
+                db.commit()  # save change to DB.
+                db.close()  # disconnect from DB.
+    return "Success!"
+#dir_json_db()
